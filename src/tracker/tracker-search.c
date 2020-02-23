cae27618aaef19a89dd85f5770a4272af2559523
Carlos Garnacho <carlosg@gnome.org>
2020-02-23 19:15:32 +0100

    Update version in DBus interfaces and bus names
    We now use Tracker3 everywhere.

--- src/tracker/tracker-search.c
+++ src/tracker/tracker-search.c
@@ -1572,7 +1572,7 @@ search_run (void)
 	disable_snippets = TRUE;
 #endif
 
-	connection = tracker_sparql_connection_bus_new ("org.freedesktop.Tracker1.Miner.Files",
+	connection = tracker_sparql_connection_bus_new ("org.freedesktop.Tracker3.Miner.Files",
 							NULL, NULL, &error);
 
 	if (!connection) {
