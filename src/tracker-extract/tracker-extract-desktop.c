sailfishos-mirror/localsearch

commit f787f74318e40548382cb0977c68322299eb850a

tracker-extract-desktop: Add support for Link type
This adds support for desktop files that point to web locations. These
files have the "Link" type and must at least have an "URL" key.

A unit test is also added.

src/tracker-extract/tracker-extract-desktop.c
@@ -30,6 +30,7 @@
 
 #define SOFTWARE_CATEGORY_URN_PREFIX "urn:software-category:"
 #define THEME_ICON_URN_PREFIX        "urn:theme-icon:"
+#define LINK_URN_PREFIX              "urn:link:"
 
 static GKeyFile *
 get_desktop_key_file (GFile   *file,
@@ -161,6 +162,45 @@ process_desktop_file (TrackerResource  *resource,
 	if (name && g_ascii_strcasecmp (type, "Application") == 0) {
 		tracker_resource_add_uri (resource, "rdf:type", "nfo:SoftwareApplication");
 		is_software = TRUE;
+	} else if (name && g_ascii_strcasecmp (type, "Link") == 0) {
+		gchar *link_url;
+
+		link_url = g_key_file_get_string (key_file, GROUP_DESKTOP_ENTRY, "URL", NULL);
+
+		if (link_url) {
+			TrackerResource *website_resource;
+			gchar *escaped_link_url;
+			gchar *link_uri;
+
+			g_strstrip (link_url);
+			escaped_link_url = g_uri_escape_string (link_url,
+								G_URI_RESERVED_CHARS_ALLOWED_IN_PATH,
+								FALSE);
+			link_uri = g_strdup_printf (LINK_URN_PREFIX "%s", escaped_link_url);
+
+			website_resource = tracker_resource_new (link_uri);
+			tracker_resource_add_uri (website_resource, "rdf:type", "nfo:Website");
+			tracker_resource_set_string (website_resource, "nie:url", link_url);
+			tracker_resource_set_take_relation (resource, "nie:isStoredAs", website_resource);
+
+			g_free (link_url);
+			g_free (escaped_link_url);
+			g_free (link_uri);
+		} else {
+			/* a Link desktop entry must have an URL */
+			gchar *uri;
+
+			uri = g_file_get_uri (file);
+			g_warning ("Link desktop entry '%s' does not have an url", uri);
+
+			g_free (uri);
+			g_free (type);
+			g_key_file_free (key_file);
+			g_strfreev (cats);
+			g_free (lang);
+			g_free (name);
+			return FALSE;
+		}
 	} else {
 		/* Invalid type, all valid types are already listed above */
 		g_warning ("Unknown desktop entry type '%s'", type);

tests/functional-tests/test-extraction-data/desktop/link-wikipedia-tracker.expected.json
@@ -1,0 +1,14 @@
+{
+    "test": {
+        "Filename": "link-wikipedia-tracker.desktop"
+    },
+    "metadata": {
+        "nie:title" : "Wikipedia Tracker",
+        "nie:isStoredAs": {
+            "@type": [
+                "nfo:Website"
+            ],
+            "nie:url": "https://en.wikipedia.org/wiki/Tracker_(search_software)"
+        }
+    }
+}

tests/functional-tests/meson.build
@@ -35,7 +35,8 @@ extractor_tests = [
   'audio/mp3-id3v2.3-empty-artist-album',
   'audio/mp3-id3v2.4-2',
   'audio/flac',
-  'desktop/application'
+  'desktop/application',
+  'desktop/link-wikipedia-tracker'
 ]
 
 if libvorbis.found()

tests/functional-tests/test-extraction-data/desktop/link-wikipedia-tracker.desktop
@@ -1,0 +1,4 @@
+[Desktop Entry]
+Type=Link
+Name=Wikipedia Tracker
+URL=https://en.wikipedia.org/wiki/Tracker_(search_software)

GNOME/tracker-miners

commit 7629b2256eb0afee822f19e04660f189b1e43b9b

tracker-extract-desktop: Fix Link type extraction
This fixes two issues introduced in commit
f787f74318e40548382cb0977c68322299eb850a:
* the uri is not correctly escaped
* a nfo:WebSite cannot have a nie:url property

The first issue is fixed by using tracker_sparql_escape_uri_printf.
The second issue is adding the nie:DataObject type to the
website_resource.

src/tracker-extract/tracker-extract-desktop.c
@@ -169,22 +169,19 @@ process_desktop_file (TrackerResource  *resource,
 
 		if (link_url) {
 			TrackerResource *website_resource;
-			gchar *escaped_link_url;
 			gchar *link_uri;
 
 			g_strstrip (link_url);
-			escaped_link_url = g_uri_escape_string (link_url,
-								G_URI_RESERVED_CHARS_ALLOWED_IN_PATH,
-								FALSE);
-			link_uri = g_strdup_printf (LINK_URN_PREFIX "%s", escaped_link_url);
+			link_uri = tracker_sparql_escape_uri_printf (LINK_URN_PREFIX "%s", link_url);
+
 
 			website_resource = tracker_resource_new (link_uri);
+			tracker_resource_add_uri (website_resource, "rdf:type", "nie:DataObject");
 			tracker_resource_add_uri (website_resource, "rdf:type", "nfo:Website");
 			tracker_resource_set_string (website_resource, "nie:url", link_url);
 			tracker_resource_set_take_relation (resource, "nie:isStoredAs", website_resource);
 
 			g_free (link_url);
-			g_free (escaped_link_url);
 			g_free (link_uri);
 		} else {
 			/* a Link desktop entry must have an URL */

tests/functional-tests/test-extraction-data/desktop/link-wikipedia-tracker.expected.json
@@ -5,7 +5,9 @@
     "metadata": {
         "nie:title" : "Wikipedia Tracker",
         "nie:isStoredAs": {
+            "@id": "urn:link:https:%2F%2Fen.wikipedia.org%2Fwiki%2FTracker_(search_software",
             "@type": [
+                "nie:DataObject",
                 "nfo:Website"
             ],
             "nie:url": "https://en.wikipedia.org/wiki/Tracker_(search_software)"